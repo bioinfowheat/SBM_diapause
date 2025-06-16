2d63a73aa88ebc92deca89fbdb1177d18059c6bb
chris <maxlongbow@gmail.com>
2025-06-16 18:40:37 +0200

    update

---
+++ parallel_versions/setup.sh
@@ -0,0 +1,5 @@
+using 
+
+for some reason, when I run this python script for graph_tools in python, it does not use multiple cores. It only runs on a single core. How do I get this script to use all 8 cores?
+
+
